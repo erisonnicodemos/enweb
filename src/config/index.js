cac9bc4161f24679ab7abd52d1fd73d86a7fb29d
Erison <erisonnicodemos@gmail.com>
2020-09-02 21:37:59 -0300

    upload

---
+++ src/config/index.js
@@ -0,0 +1,49 @@
+module.exports = {
+
+    author: "@erisonnicodemos",
+    siteTitle: "Erison Nicodemos Web Developer",
+    siteShortTitle: "enweb", // Used as logo text in header, footer, and splash screen
+    siteDescription: "Ola! sou Erison, Devenvolvedor com experiência de 7 anos desenvolvendo soluções em tecnologia para Internet",
+    siteUrl: "https://konstantin.digital",
+    siteLanguage: "en_US",
+    siteIcon: "src/content/favicon.png", // Relative to gatsby-config file
+
+    mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantin.muenster&api_key=51t0yukvezbsj6vz5h46icya39agdbxw1snykm3v",
+    shownArticles: 3,
+    
+    socialMedia: [
+          {
+            name: "Linkedin",
+            url: "https://www.linkedin.com/in/erisonnicodemos/"
+        },
+        {
+            name: "Github",
+            url: "https://github.com/erisonnicodemos"
+        },
+        {
+            name: "Behance",
+            url: "https://www.behance.net/erisonnico5697"
+        },
+    ],
+  
+    navLinks: {
+        menu: [
+                        {
+                name: "About Me",
+                url: "/#about",
+            },
+            // {
+            //     name: "Projects",
+            //     url: "/#projects",
+            // },
+        ],
+        button: {
+            name: "Contact",
+            url: "/#contact",
+        }
+    },
+
+    footerLinks: [
+       
+    ]
+}
